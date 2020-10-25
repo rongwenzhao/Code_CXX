0b22e1a952ad285bdedc3a557c2fa12fa3c6e861
rongwenzhao <rong_wz@126.com>
2020-10-25 15:28:39 +0800

    通过遍历nb_streams来获取音频流跟视频流。

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,6 +15,15 @@ extern "C" {
 
 }
 
+/**
+ * 將AVRational這樣的分數轉爲帶小數點的浮點數。
+ * @param r
+ * @return
+ */
+static double r2d(AVRational r) {
+    return r.num == 0 || r.den == 0 ? 0 : (double) r.num / (double) r.den;
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_nick_play_MainActivity_stringFromJNI(
@@ -29,8 +38,8 @@ Java_com_nick_play_MainActivity_stringFromJNI(
     //打开文件
 
     AVFormatContext *ic = NULL;
-//    char path[] = "/sdcard/1280x720_1.mp4";
-    char path[] = "/sdcard/1280x720_1.flv";
+    char path[] = "/sdcard/1280x720_1.mp4";
+//    char path[] = "/sdcard/1280x720_1.flv";
     int re = avformat_open_input(&ic, path, 0, 0);
     if (re != 0) {
         LOGW("avformat_open_input failed!:%s", av_err2str(re));
@@ -45,6 +54,42 @@ Java_com_nick_play_MainActivity_stringFromJNI(
     }
 
     LOGW("文件信息 duration = %lld nb_streams = %d", ic->duration, ic->nb_streams);
+
+    //打印音視頻相關的信息
+    int fps = 0;
+    int videoStream = 0;
+    int audioStream = 1;
+
+    for (int i = 0; i < ic->nb_streams; i++) {
+        AVStream *as = ic->streams[i];
+        if (as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            LOGW("視頻數據");
+            videoStream = i;
+            //将帧率转换为浮点数
+            fps = r2d(as->avg_frame_rate);
+
+            LOGW("fps = %d, width=%d height=%d codeid=%d format=%d", fps,
+                 as->codecpar->width,
+                 as->codecpar->height,
+                 as->codecpar->codec_id,
+                 as->codecpar->format
+            );
+            //codec_id=28 AV_CODEC_ID_H264
+            //format=0 AVPixelFormat AV_PIX_FMT_YUV420P
+
+
+        } else if (as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            LOGW("音頻數據");
+            audioStream = i;
+            LOGW("sample_rate=%d channels=%d sample_format=%d",
+                 as->codecpar->sample_rate,
+                 as->codecpar->channels,
+                 as->codecpar->format
+            );
+            //AVSampleFormat sample_format=8 AV_SAMPLE_FMT_FLTP
+
+        }
+    }
 
 
     //關閉上下文
